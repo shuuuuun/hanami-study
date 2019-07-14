369dfde0cd5dc5c817fef81f927748f13718e2a7
motoki-shun <motoki-shun@kayac.com>
2019-07-14 21:11:12 +0900

    test

--- spec/api/views/books/index_spec.rb
+++ spec/api/views/books/index_spec.rb
@@ -1,10 +1,16 @@
 RSpec.describe Api::Views::Books::Index, type: :view do
-  let(:exposures) { Hash[format: :json] }
-  let(:template)  { Hanami::View::Template.new('apps/api/templates/books/show.json.erb') }
-  let(:view)      { described_class.new(template, exposures) }
+  let(:books)     { Array.new }
+  let(:exposures) { Hash[format: :json, books: books] }
+  let(:view)      { described_class.new(nil, exposures) }
   let(:rendered)  { view.render }
+  let(:rendered_json) { JSON.parse(rendered) }
 
   it 'exposes #format' do
     expect(view.format).to eq exposures.fetch(:format)
+  end
+
+  it 'response' do
+    # binding.pry
+    expect(rendered_json).to eq []
   end
 end
---
+++ spec/api/views/books/show_spec.rb
@@ -0,0 +1,21 @@
+RSpec.describe Api::Views::Books::Show, type: :view do
+  let(:book)      { double('book') }
+  let(:exposures) { Hash[format: :json, book: book] }
+  let(:template)  { Hanami::View::Template.new('apps/api/templates/books/show.json.erb') }
+  let(:view)      { described_class.new(template, exposures) }
+  let(:rendered)  { view.render }
+  let(:rendered_json) { JSON.parse(rendered) }
+
+  before do
+    book.stub(:id) { 1 }
+  end
+
+  it 'exposes #format' do
+    expect(view.format).to eq exposures.fetch(:format)
+  end
+
+  it 'response' do
+    # binding.pry
+    expect(rendered_json['id'].to_i).to eq 1
+  end
+end
