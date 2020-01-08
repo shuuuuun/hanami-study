65e4bfc7eb22187fe9c3a44154de089c2fab7a4e
motoki-shun <shuuuuuny+github@gmail.com>
2020-01-08 23:51:39 +0900

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,3 +4,8 @@ require_relative '../config/environment'
 Hanami.boot
 
 BookRepository.new.create(title: 'hoge')
+UserRepository.new.create(email: 'hoge@example.com', password: 'password')
+UserRepository.new.create(email: 'fuga@example.com', password: 'password')
+UserRepository.new.create(email: 'piyo@example.com', password: 'password')
+UserRepository.new.create(email: 'poyo@example.com', password: 'password')
+UserRepository.new.create(email: 'moga@example.com', password: 'password')
