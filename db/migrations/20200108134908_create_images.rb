a2e43cf951cc029a236163b4222bf9bceb68d39a
motoki-shun <shuuuuuny+github@gmail.com>
2020-01-08 22:53:31 +0900

    add user_id,title,image colmun

--- db/migrations/20200108134908_create_images.rb
+++ db/migrations/20200108134908_create_images.rb
@@ -2,6 +2,9 @@ Hanami::Model.migration do
   change do
     create_table :images do
       primary_key :id
+      foreign_key :user_id, :users, on_delete: :cascade, null: false
+      column :title, String, null: false
+      column :image, String, null: false
 
       column :created_at, DateTime, null: false
       column :updated_at, DateTime, null: false
