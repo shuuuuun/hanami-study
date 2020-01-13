eb6413bf508146d793165a899d65f98d8709208f
motoki-shun <shuuuuuny+github@gmail.com>
2020-01-13 15:39:49 +0900

    remove users#index

--- apps/web/config/routes.rb
+++ apps/web/config/routes.rb
@@ -4,15 +4,18 @@
 # Example:
 # get '/hello', to: ->(env) { [200, {}, ['Hello from Hanami!']] }
 get '/ping', to: ->(env) { [200, {}, ['pong']] }
+
 get '/', to: 'home#index'
-get '/users', to: 'users#index'
+
 get '/images', to: 'images#index'
 get '/images/:id', to: 'images#show'
 get '/images/:id/edit', to: 'images#edit'
 get '/images/new', to: 'images#new'
 post '/images', to: 'images#create'
+
 get '/users/new', to: 'users#new'
 post '/users', to: 'users#create'
+
 get '/sessions/new', to: 'sessions#new'
 post '/sessions', to: 'sessions#create'
 delete '/sessions/:id', to: 'sessions#destroy'
