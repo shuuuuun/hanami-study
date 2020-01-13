f7a1fb323b08793bd333a12932d5208d9f4afde7
motoki-shun <shuuuuuny+github@gmail.com>
2020-01-13 15:50:57 +0900

    update routing, signup/login, use resources

--- apps/web/config/routes.rb
+++ apps/web/config/routes.rb
@@ -7,15 +7,16 @@ get '/ping', to: ->(env) { [200, {}, ['pong']] }
 
 get '/', to: 'home#index'
 
-get '/images', to: 'images#index'
-get '/images/:id', to: 'images#show'
-get '/images/:id/edit', to: 'images#edit'
-get '/images/new', to: 'images#new'
-post '/images', to: 'images#create'
+# get '/images', to: 'images#index'
+# get '/images/:id', to: 'images#show'
+# get '/images/:id/edit', to: 'images#edit'
+# get '/images/new', to: 'images#new'
+# post '/images', to: 'images#create'
+resources :images, only: [:index, :show, :new, :create, :edit]
 
-get '/users/new', to: 'users#new'
-post '/users', to: 'users#create'
+get '/signup', to: 'users#new'
+post '/signup', to: 'users#create'
 
-get '/sessions/new', to: 'sessions#new'
-post '/sessions', to: 'sessions#create'
-delete '/sessions/:id', to: 'sessions#destroy'
+get '/login', to: 'sessions#new'
+post '/login', to: 'sessions#create'
+delete '/logout', to: 'sessions#destroy'
