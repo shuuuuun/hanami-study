e0fd8de1a61f11ec47add4dbe6941517bfbfeee9
motoki-shun <shuuuuuny+github@gmail.com>
2020-01-14 22:14:06 +0900

    impliment users#create

--- apps/web/controllers/users/create.rb
+++ apps/web/controllers/users/create.rb
@@ -1,11 +1,28 @@
-module Web
-  module Controllers
-    module Users
-      class Create
-        include Web::Action
+module Web::Controllers::Users
+  class Create
+    include Web::Action
 
-        def call(params)
-        end
+    expose :user
+
+    params do
+      required(:user).schema do
+        required(:email).filled(:str?)
+        required(:password).filled(:str?)
+        # required(:email).value(:filled?)
+        # required(:password).value(:filled?, size?: 8..40, format?: /\A[\w!$%@#123]+\z/).confirmation
+        # required(:password_confirmation).filled(:str?)
+      end
+      # required(:email).filled(:str?, format?: /@/).confirmation
+      # required(:password).filled(:str?).confirmation
+    end
+
+    def call(params)
+      if params.valid?
+        @user = UserRepository.new.create(params[:user])
+
+        redirect_to routes.images_path
+      else
+        self.status = 422
       end
     end
   end
