a383860d44d94043dbd0f5cc16e9e6c1f3bea223
motoki-shun <shuuuuuny+github@gmail.com>
2020-01-25 23:34:17 +0900

    images

--- apps/web/controllers/images/create.rb
+++ apps/web/controllers/images/create.rb
@@ -1,10 +1,30 @@
 module Web::Controllers::Images
   class Create
     include Web::Action
+    before :reject_unless_authenticated
+
+    MEGABYTE = 1024 ** 2
+
+    params do
+      required(:image).schema do
+        required(:title).filled(:str?)
+        required(:image)  { size?(1..(5 * MEGABYTE)) }
+      end
+    end
 
     def call(params)
-      # TODO: create image
       # TODO: uplaod image
+      image = ImageRepository.new.create(params[:image].merge(user_id: current_user.id))
+      # Hanami.logger.debug "image: #{image.inspect}"
+      if params.valid? && image
+        # flash[:notice] = 'logged in.'
+        redirect_to routes.images_path
+      else
+        Hanami.logger.info "invalid params. errors: #{params.errors}"
+        self.status = 400
+        # flash[:error] = 'invalid params.'
+        render :new
+      end
     end
   end
 end
--- apps/web/controllers/images/update.rb
+++ apps/web/controllers/images/update.rb
@@ -2,8 +2,25 @@ module Web::Controllers::Images
   class Update
     include Web::Action
 
+    params do
+      required(:image).schema do
+        required(:title).filled(:str?)
+        # required(:image).filled(:file?)
+      end
+    end
+
     def call(params)
-      # TODO: update image
+      image = ImageRepository.new(params[:image])
+      Hanami.logger.debug "image: #{image.inspect}"
+      if params.valid? && image
+        # flash[:notice] = 'logged in.'
+        redirect_to routes.images_path
+      else
+        Hanami.logger.info "invalid params. errors: #{params.errors}"
+        self.status = 400
+        flash[:error] = 'invalid params.'
+        render :new
+      end
     end
   end
 end
