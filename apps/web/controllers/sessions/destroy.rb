c31c63c2f783aca70ac2b6edfaf1a29f5438bf15
motoki-shun <shuuuuuny+github@gmail.com>
2020-01-25 19:40:32 +0900

    session

--- apps/web/controllers/sessions/destroy.rb
+++ apps/web/controllers/sessions/destroy.rb
@@ -1,9 +1,13 @@
 module Web::Controllers::Sessions
   class Destroy
     include Web::Action
-    before :reject_if_authenticated
+    before :reject_unless_authenticated
 
     def call(params)
+      Hanami.logger.debug "current_user: #{current_user.inspect}"
+      logout
+      flash[:notice] = 'logged out.'
+      redirect_to routes.root_path
     end
   end
 end
