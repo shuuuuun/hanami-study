b8f21bd62be821819f6c8f3a2f7efdb0e859ffcc
motoki-shun <shuuuuuny+github@gmail.com>
2020-01-26 20:58:32 +0900

    images show

--- apps/web/controllers/images/show.rb
+++ apps/web/controllers/images/show.rb
@@ -2,9 +2,12 @@ module Web::Controllers::Images
   class Show
     include Web::Action
     before :reject_unless_authenticated
+    expose :_image
 
     def call(params)
-      # TODO: show image
+      # imgタグのヘルパーと衝突するためアンスコを付けてる
+      # ref. https://guides.hanamirb.org/helpers/assets/#image
+      @_image = ImageRepository.new.find(params[:id])
     end
   end
 end
