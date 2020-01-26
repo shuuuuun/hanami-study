ded3e97aec1fd9e49d3b5a4f86c2540253618438
motoki-shun <shuuuuuny+github@gmail.com>
2020-01-26 20:10:58 +0900

    fix column name for shrine, image_data.

--- db/migrations/20200108134908_create_images.rb
+++ db/migrations/20200108134908_create_images.rb
@@ -4,7 +4,7 @@ Hanami::Model.migration do
       primary_key :id
       foreign_key :user_id, :users, on_delete: :cascade, null: false
       column :title, String, null: false
-      column :image, String
+      column :image_data, String
 
       column :created_at, DateTime, null: false
       column :updated_at, DateTime, null: false
