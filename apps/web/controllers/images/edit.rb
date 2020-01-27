f9159fc9702518296c4938eddeda2e68430e5be5
motoki-shun <shuuuuuny+github@gmail.com>
2020-01-27 22:25:00 +0900

    edit image

--- apps/web/controllers/images/edit.rb
+++ apps/web/controllers/images/edit.rb
@@ -2,9 +2,12 @@ module Web::Controllers::Images
   class Edit
     include Web::Action
     before :reject_unless_authenticated
+    expose :_image
 
     def call(params)
-      # TODO: edit image
+      # imgタグのヘルパーと衝突するためアンスコを付けてる
+      # ref. https://guides.hanamirb.org/helpers/assets/#image
+      @_image = ImageRepository.new.find(params[:id])
     end
   end
 end
--- apps/web/controllers/images/update.rb
+++ apps/web/controllers/images/update.rb
@@ -3,19 +3,22 @@ module Web::Controllers::Images
     include Web::Action
     before :reject_unless_authenticated
 
-    params do
-      required(:image).schema do
-        required(:title).filled(:str?)
-        # required(:image).filled(:file?)
-      end
-    end
+    # params do
+    #   required(:image).schema do
+    #     required(:title).filled(:str?)
+    #     # required(:image).filled(:file?)
+    #   end
+    # end
 
     def call(params)
-      image = ImageRepository.new(params[:image])
-      Hanami.logger.debug "image: #{image.inspect}"
+      image_params = params[:image]
+      Hanami.logger.debug "image_params: #{image_params.inspect}"
+      image_params = image_params.merge(image: ::File.open(image_params[:image][:tempfile])) if image_params[:image]
+      image = Image.new(image_params)
+      image = ImageRepository.new.update(params[:id], image)
       if params.valid? && image
-        # flash[:notice] = 'logged in.'
-        redirect_to routes.images_path
+        flash[:notice] = 'edited image.'
+        redirect_to routes.image_path(image.id)
       else
         Hanami.logger.info "invalid params. errors: #{params.errors}"
         self.status = 400
