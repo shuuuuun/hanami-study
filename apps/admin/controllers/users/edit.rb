7e71a2525ed8fcaf8d640046c804134d6ecbbfca
motoki-shun <shuuuuuny+github@gmail.com>
2020-03-01 16:56:33 +0900

    users#edit

---
+++ apps/admin/controllers/users/edit.rb
@@ -0,0 +1,12 @@
+module Admin
+  module Controllers
+    module Users
+      class Edit
+        include Admin::Action
+
+        def call(params)
+        end
+      end
+    end
+  end
+end
---
+++ apps/admin/views/users/edit.rb
@@ -0,0 +1,9 @@
+module Admin
+  module Views
+    module Users
+      class Edit
+        include Admin::View
+      end
+    end
+  end
+end
