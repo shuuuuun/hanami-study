570b6cb1918bde2044f27c5f500e89e97564dd14
motoki-shun <shuuuuuny+github@gmail.com>
2020-03-01 17:04:43 +0900

    use resources in routes

--- apps/admin/config/routes.rb
+++ apps/admin/config/routes.rb
@@ -5,10 +5,15 @@
 # get '/hello', to: ->(env) { [200, {}, ['Hello from Hanami!']] }
 get '/', to: 'home#index', as: :root
 get '/home', to: 'home#index'
-get '/users', to: 'users#index'
-get '/users/:id', to: 'users#show'
-get '/users/new', to: 'users#new'
-get '/users/:id/edit', to: 'users#edit'
-post '/users', to: 'users#create'
-patch '/users/:id', to: 'users#update'
-get '/users/delete', to: 'users#delete'
+
+# get '/users', to: 'users#index'
+# get '/users/:id', to: 'users#show'
+# get '/users/new', to: 'users#new'
+# get '/users/:id/edit', to: 'users#edit'
+# post '/users', to: 'users#create'
+# patch '/users/:id', to: 'users#update'
+# get '/users/delete', to: 'users#delete'
+resources :users
+
+# resources :images
+# resources :books
